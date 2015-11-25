57df377ff1f2e031a19b27796d934cc4fa4ef46e
Aitor Magán <amagan@conwet.com>
2015-11-25 13:50:50 +0100

    Change download URL

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     author='CoNWeT Lab.',
     author_email='amagan@conwet.com',
     url='https://conwet.fi.upm.es',
-    download_url='https://github.com/conwetlab/ckanext-datarequests/releases/tag/v' + version,
+    download_url='https://github.com/conwetlab/ckanext-datarequests/tarball/v' + version,
     license='GNU Affero General Public License',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.datarequests'],
