89e4418201125d7ff984fbb7782220e3510ba7c8
ThrawnCA <shell_layer-github@yahoo.com.au>
2019-07-04 11:02:48 +1000

    CHG-4018 (#4)
    * Add missing float cast
    
    * Add French translations
    
    * Added configurable required description
    Added block offering_description to datarequest_form template so other templates can override the block
    
    * Updated readme with ckan.datarequests.description_required

--- ckanext/datarequests/tests/test_selenium.py
+++ ckanext/datarequests/tests/test_selenium.py
@@ -467,7 +467,7 @@ class TestSelenium(unittest.TestCase):
         self.check_datarequest(datarequest_id, datarequest_title,
                                datarequest_description, False, True)
 
-    @unittest.skipIf('.'.join(os.environ.get('CKANVERSION', '2.7').split('.')[:-1]) >= 2.8, 
+    @unittest.skipIf(float('.'.join(os.environ.get('CKANVERSION', '2.7').split('.')[:-1])) >= 2.8, 
                      'This test cannot be run in CKAN >= 2.8 because elements in combo boxes cannot be selected')
     def test_close_datarequest_with_organization_and_accepted_dataset(self):
 
