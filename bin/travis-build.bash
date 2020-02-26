b5620c28bbf5a88b04a69dfef426416c928a0342
Aitor Magán García <aitormagan@users.noreply.github.com>
2020-02-26 10:18:14 +0100

    Update jetty config file

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -21,7 +21,7 @@ echo "Setting up Solr..."
 # on Travis single-core still.
 # see https://github.com/ckan/ckan/issues/2972
 sed -i -e 's/solr_url.*/solr_url = http:\/\/127.0.0.1:8983\/solr/' ckan/test-core.ini
-printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
+printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty8
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty8 restart
 
