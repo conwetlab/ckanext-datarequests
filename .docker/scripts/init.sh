c7cff1503bf5156925a2ec5a977b3f5e207f58fb
ThrawnCA <shell_layer-github@yahoo.com.au>
2020-07-27 12:37:07 +1000

    initialise QA tables

--- .docker/scripts/init.sh
+++ .docker/scripts/init.sh
@@ -18,5 +18,8 @@ CKAN_USER_EMAIL="${CKAN_USER_EMAIL:-admin@localhost}"
 # Initialise the Comments database tables
 paster --plugin=ckanext-ytp-comments initdb --config=/app/ckan/default/production.ini
 
+# Initialise the QA database tables
+paster --plugin=ckanext-qa qa init --config=/app/ckan/default/production.ini
+
 # Create some base test data
 . /app/scripts/create-test-data.sh
