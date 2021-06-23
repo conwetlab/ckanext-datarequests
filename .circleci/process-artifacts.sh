aff9a31c1f47cb4e9cbfbe600a4a43be9e44eddc
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-06-23 13:08:08 +1000

    [QOL-8053] fix mail path

--- .circleci/process-artifacts.sh
+++ .circleci/process-artifacts.sh
@@ -6,10 +6,10 @@ set -e
 
 # Create screenshots directory in case it was not created before. This is to
 # avoid this script to fail when copying artifacts.
-ahoy cli "mkdir -p test/screenshots test/emails"
+ahoy cli "mkdir -p test/screenshots test/emails mail"
 
 # Copy from the app container to the build host for storage.
 mkdir -p /tmp/artifacts/behave
 docker cp "$(docker-compose ps -q ckan)":/app/test/screenshots /tmp/artifacts/behave/
 docker cp "$(docker-compose ps -q ckan)":/app/test/emails /tmp/artifacts/behave/
-docker cp "$(docker-compose ps -q ckan)":"/app/mail/*" /tmp/artifacts/behave/emails/
+docker cp "$(docker-compose ps -q ckan)":/app/mail /tmp/artifacts/behave/emails/
