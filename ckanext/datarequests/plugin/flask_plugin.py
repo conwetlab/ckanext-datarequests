058ef0320078b1a30f561be256039520a6796914
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-11-09 16:28:06 +1000

    [QOL-7970] replace f-strings with Python 2 compatible syntax

--- ckanext/datarequests/plugin/flask_plugin.py
+++ ckanext/datarequests/plugin/flask_plugin.py
@@ -18,52 +18,52 @@ class MixinPlugin(p.SingletonPlugin):
     def get_blueprint(self):
         rules = [
             (
-                f"/{constants.DATAREQUESTS_MAIN_PATH}",
+                "/" + constants.DATAREQUESTS_MAIN_PATH,
                 "index",
                 ui_controller.index,
             ),
             (
-                f"/{constants.DATAREQUESTS_MAIN_PATH}/new",
+                "/{}/new".format(constants.DATAREQUESTS_MAIN_PATH),
                 "new",
                 ui_controller.new,
             ),
             (
-                f"/{constants.DATAREQUESTS_MAIN_PATH}/<id>",
+                "/{}/<id>".format(constants.DATAREQUESTS_MAIN_PATH),
                 "show",
                 ui_controller.show,
             ),
             (
-                f"/{constants.DATAREQUESTS_MAIN_PATH}/edit/<id>",
+                "/{}/edit/<id>".format(constants.DATAREQUESTS_MAIN_PATH),
                 "update",
                 ui_controller.update,
             ),
             (
-                f"/{constants.DATAREQUESTS_MAIN_PATH}/delete/<id>",
+                "/{}/delete/<id>".format(constants.DATAREQUESTS_MAIN_PATH),
                 "delete",
                 ui_controller.delete,
             ),
             (
-                f"/{constants.DATAREQUESTS_MAIN_PATH}/close/<id>",
+                "/{}/close/<id>".format(constants.DATAREQUESTS_MAIN_PATH),
                 "close",
                 ui_controller.close,
             ),
             (
-                f"/{constants.DATAREQUESTS_MAIN_PATH}/follow/<datarequest_id>",
+                "/{}/follow/<datarequest_id>".format(constants.DATAREQUESTS_MAIN_PATH),
                 "follow",
                 ui_controller.follow,
             ),
             (
-                f"/{constants.DATAREQUESTS_MAIN_PATH}/unfollow/<datarequest_id>",
+                "/{}/unfollow/<datarequest_id>".format(constants.DATAREQUESTS_MAIN_PATH),
                 "unfollow",
                 ui_controller.unfollow,
             ),
             (
-                f"/organization/{constants.DATAREQUESTS_MAIN_PATH}/<id>",
+                "/organization/{}/<id>".format(constants.DATAREQUESTS_MAIN_PATH),
                 "organization",
                 ui_controller.organization,
             ),
             (
-                f"/user/{constants.DATAREQUESTS_MAIN_PATH}/<id>",
+                "/user/{}/<id>".format(constants.DATAREQUESTS_MAIN_PATH),
                 "user",
                 ui_controller.user,
             ),
@@ -73,12 +73,13 @@ class MixinPlugin(p.SingletonPlugin):
             rules.extend(
                 [
                     (
-                        f"/{constants.DATAREQUESTS_MAIN_PATH}/comment/<id>",
+                        "/{}/comment/<id>".format(constants.DATAREQUESTS_MAIN_PATH),
                         "comment",
                         ui_controller.comment,
                     ),
                     (
-                        f"/{constants.DATAREQUESTS_MAIN_PATH}/comment/<datarequest_id>/delete/<comment_id>",
+                        "/{}/comment/<datarequest_id>/delete/<comment_id>".format(
+                            constants.DATAREQUESTS_MAIN_PATH),
                         "delete_comment",
                         ui_controller.delete_comment,
                     ),
